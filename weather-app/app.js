656e2611c9d9a1191fca06b7ec2cb98affbaadab
alexbarksdale <ale.barksdale@gmail.com>
2020-04-20 23:40:59 -0500

    Error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,18 +2,28 @@ const request = require('request');
 require('dotenv').config();
 
 const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API}&query=37.8267,-122.4233&units=f`;
-
 request({ url, json: true }, (err, res) => {
-    const result = res.body.current;
-    console.log(
-        `It is currently ${result.temperature}F degrees out. There is a ${result.precip}% chance of precip`
-    );
+    if (err) {
+        console.log(err);
+    } else if (res.body.error.type === 'missing_query') {
+        console.log('Unable to find location');
+    } else {
+        const result = res.body.current;
+        console.log(
+            `It is currently ${result.temperature}F degrees out. There is a ${result.precip}% chance of precip`
+        );
+    }
 });
 
 const geourl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_API}&limit=1`;
-
 request({ url: geourl, json: true }, (err, res) => {
-    const lat = res.body.features[0].center[1];
-    const lng = res.body.features[0].center[0];
-    console.log(lat, lng);
+    if (err) {
+        console.log(err);
+    } else if (res.body.features.length === 0) {
+        console.log('Unable to find location');
+    } else {
+        const lat = res.body.features[0].center[1];
+        const lng = res.body.features[0].center[0];
+        console.log(lat, lng);
+    }
 });
