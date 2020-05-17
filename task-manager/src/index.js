acba0fd22859e8d19d321bf062384f2e30e33c11
Alex <dev.alexbarksdale@gmail.com>
2020-05-17 01:49:32 -0500

    Async await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,78 +9,71 @@ const PORT = process.env.PORT || 3000;
 
 // Parse incoming JSON to an object
 app.use(express.json());
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
-    user.save()
-        .then(() => {
-            res.status(201).send(user);
-        })
-        .catch(() => {
-            res.status(400).send('Failed to save user to the DB');
-        });
+
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send('Failed to save user to the DB');
+    }
 });
 
-app.get('/users', (_, res) => {
-    // Fetches all users in the DB
-    User.find({})
-        .then((users) => {
-            res.send(users);
-        })
-        .catch(() => {
-            res.status(500).send('Failed to fetch users');
-        });
+app.get('/users', async (_, res) => {
+    try {
+        // Fetches all users in the DB
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(500).send('Failed to fetch users');
+    }
 });
 
-app.get('/users/:id', (req, res) => {
-    User.findById(req.params.id)
-        .then((user) => {
-            if (!user) {
-                return res.status(404).send();
-            }
+app.get('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
 
-            res.send(user);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send();
-        });
+        if (!user) return res.status(404).send();
+        res.send(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send();
+    }
 });
 
-app.get('/tasks', (_, res) => {
-    Task.find({})
-        .then((task) => {
-            res.send(task);
-        })
-        .catch(() => {
-            res.status(500).send('Failed to find task');
-        });
+app.get('/tasks', async (_, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);
+    } catch (err) {
+        res.status(500).send('Failed to find task');
+    }
 });
 
-app.get('/tasks/:id', (req, res) => {
-    Task.findById(req.params.id)
-        .then((task) => {
-            if (!task) {
-                return res.status(404).send();
-            }
+app.get('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
 
-            res.send(task);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send();
-        });
+        if (!task) return res.status(404).send();
+        res.send(task);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send();
+    }
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
 
-    task.save()
-        .then(() => {
-            res.status(201).send(task);
-        })
-        .catch((err) => {
-            res.status(400).send('Failed to create a task', err);
-        });
+    try {
+        await task.save();
+
+        res.status(201).send(task);
+    } catch (err) {
+        res.status(400).send('Failed to create a task', err);
+    }
 });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
