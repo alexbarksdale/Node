4d2a6007e3c3170ba374b747e9bfe1c2202cd722
Alex <dev.alexbarksdale@gmail.com>
2020-05-17 16:39:39 -0500

    jwt

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -12,4 +12,21 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+const jwt = require('jsonwebtoken');
+
+const test = async () => {
+    const token = jwt.sign(
+        {
+            _id: '123',
+        },
+        'test'
+    );
+
+    jwt.verify(token, 'test');
+
+    console.log(token);
+};
+
+test();
+
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
